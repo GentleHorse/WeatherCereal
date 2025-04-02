a3fc8b261c2666edda31cb88e5ec935cdac5d79f
GentleHorse <anch.abc.t@gmail.com>
2025-04-02 23:51:05 +0200

    Test gsap animation with weather sweet

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -1,39 +1,79 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { OrbitControls, Text3D, Center } from "@react-three/drei";
 import { Perf } from "r3f-perf";
 import { Physics, RigidBody, CuboidCollider } from "@react-three/rapier";
+import gsap from "gsap";
+
 import PostProcessingEffects from "./postprocessing/PostProcessingEffects.jsx";
 import FallingWeatherIcons from "./weatherIcons/FallingWeatherIcons.jsx";
 import CustomEnvironment from "./customEnvironment/CustomEnvironment.jsx";
 import Stage from "./stage/Stage.jsx";
 
 const WEATHER_SWEET_UNIT_SIZE = 0.15;
-const WEATHER_SWEET_UNIT_GAP = 0.15;
+const WEATHER_SWEET_UNIT_GAP = 0.05;
 
 export default function Experience({
   weatherData,
   city,
   showDataRelatedModels,
 }) {
+  const weatherSweet = useRef();
+
+  function hoverStartAnimation() {
+    gsap.to(weatherSweet.current.scale, {
+      duration: 1.0,
+      ease: "bounce.out",
+      x: 1.25,
+      y: 1.25,
+      z: 1.25,
+    });
+    
+
+    gsap.to(weatherSweet.current.position, {
+      duration: 1.0,
+      ease: "bounce.out",
+      y: 0.25,
+    });
+  }
+
+  function hoverEndAnimation() {
+    gsap.to(weatherSweet.current.scale, {
+      duration: 1.0,
+      ease: "bounce.out",
+      x: 1.0,
+      y: 1.0,
+      z: 1.0,
+    });
+
+    gsap.to(weatherSweet.current.position, {
+      duration: 1.0,
+      ease: "bounce.out",
+      y: 0.0,
+    });
+  }
+
   return (
     <>
       <Perf position="top-left" />
       <OrbitControls makeDefault />
-      <axesHelper visible={true} />
+      {/* <axesHelper visible={true} /> */}
 
       <CustomEnvironment backgroundColor={true} />
 
-      <PostProcessingEffects depthOfField={false} />
+      <PostProcessingEffects depthOfField={true} />
 
-      <WeatherSweet
-        position={[1.5, 0, 3.5]}
-        unitSize={WEATHER_SWEET_UNIT_SIZE}
-        unitGap={WEATHER_SWEET_UNIT_GAP}
-      />
+      <group ref={weatherSweet} position={[1.25, 0, 2.5]}>
+        <WeatherSweet
+          onPointerEnter={hoverStartAnimation}
+          onPointerLeave={hoverEndAnimation}
+          unitSize={WEATHER_SWEET_UNIT_SIZE}
+          unitGap={WEATHER_SWEET_UNIT_GAP}
+        />
+      </group>
 
-      <Physics debug={true} gravity={[0, -1.625, 0]}>
+      <Physics debug={false} gravity={[0, -1.625, 0]}>
         <RigidBody type="fixed" restitution={0.1} position={[0, -0.5, 0]}>
           <CuboidCollider restitution={0.1} args={[1000, 0.1, 1000]} />
         </RigidBody>
@@ -107,7 +147,7 @@ function WeatherSweet({ unitSize, unitGap, ...props }) {
     const unitGapX = unitSize + unitGap;
     const unitGapY = unitSize * 1.33 + unitGap;
     const unitGapZ = unitSize + unitGap;
-    const totalUnitNum = 4 * 4 * 3;
+    const totalUnitNum = 4 * 3 * 4;
 
     for (let i = 0; i < totalUnitNum; i++) {
       const sweetUnit = {
@@ -139,6 +179,16 @@ function WeatherSweet({ unitSize, unitGap, ...props }) {
             <meshStandardMaterial color="#C1C1C1" />
           </mesh>
         ))}
+      <mesh
+        position={[
+          ((unitSize + unitGap) * (4 - 1)) / 2,
+          ((unitSize * 1.33 + unitGap) * (3 - 1)) / 2,
+          -((unitSize + unitGap) * (4 - 1)) / 2,
+        ]}
+      >
+        <boxGeometry args={[0.8, 0.8, 0.8]} />
+        <meshStandardMaterial opacity={0.45} transparent />
+      </mesh>
     </group>
   );
 }
