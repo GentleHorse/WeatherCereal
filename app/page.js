5d0add40693d39364fedf709ecac625772d12a7b
GentleHorse <anch.abc.t@gmail.com>
2025-04-05 05:38:11 +0200

    loading.js - updated loading animation

--- app/page.js
+++ app/page.js
@@ -1,9 +1,6 @@
 import ThreeScene from "@/components/ThreeScene.jsx";
-import { resolve } from "styled-jsx/css";
 
-export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 1500));
-
+export default function Home() {
   return (
     <main className="relative flex justify-center items-center h-screen z-0 bg-[#1C1C1C]">
       <ThreeScene />
