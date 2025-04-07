8a112a8804d35c233bc6fd5983b3947831085414
GentleHorse <anch.abc.t@gmail.com>
2025-04-07 02:46:30 +0200

    Updated the loader animation

--- components/loadingScene/LoadingScene.jsx
+++ components/loadingScene/LoadingScene.jsx
@@ -5,8 +5,8 @@ export default function LoadingScene() {
     <>
       <Html fullscreen>
         <div className="flex flex-col gap-10 items-center justify-center h-screen bg-[#1C1C1C]">
-          <span className="loader threeD"></span>
-          <p className="w-[200px] text-center text-[10px] xl:text-sm font-mono text-[#FEDFE1]">Loading .....</p>
+          <span className="loader"></span>
+          <p className="loaderText w-[200px] text-center text-[10px] xl:text-sm font-mono text-[#FEDFE1]">Loading .....</p>
         </div>
       </Html>
     </>
