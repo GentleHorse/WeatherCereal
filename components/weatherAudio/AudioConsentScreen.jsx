4c5875621fa1e56d6175eb6dd42a7cf30ab892a5
GentleHorse <anch.abc.t@gmail.com>
2025-04-08 15:10:01 +0200

    Styled audio consent screen component

--- components/weatherAudio/AudioConsentScreen.jsx
+++ components/weatherAudio/AudioConsentScreen.jsx
@@ -7,29 +7,36 @@ export default function AudioConsentScreen() {
 
   if (!hasInteracted) {
     return (
-      <div className="absolute m-auto inset-0 flex flex-col items-center justify-center w-[90vw] h-[90vh] pt-[5vh] rounded-2xl backdrop-blur-md  bg-[#333333]/70 text-white z-50">
-        <h1 className="text-xl mb-4">
-          Would you like to enable ambient sound?
-        </h1>
-        <div className="flex gap-4">
-          <button
-            onClick={() => {
-              changeAudioEnabled(true);
-              setHasInteracted(true);
-            }}
-            className="px-4 py-2 bg-emerald-600 rounded-lg"
-          >
-            Yes, with sound
-          </button>
-          <button
-            onClick={() => {
-              changeAudioEnabled(false);
-              setHasInteracted(true);
-            }}
-            className="px-4 py-2 bg-slate-500 rounded-lg"
-          >
-            No, silent
-          </button>
+      <div className="absolute m-auto px-10 py-[10vh] w-[90vw] h-[90vh] inset-0 flex flex-col items-center justify-center gap-10 rounded-2xl backdrop-blur-md bg-[#333333]/70 text-white z-50">
+        <img
+          src="/images/play-state/audio-on.svg"
+          className="w-[95px] h-[95px] opacity-90"
+        />
+
+        <div className="flex flex-col items-center">
+          <h1 className="mx-8 mb-6 text-md text-center">
+            Would you like to enable ambient sound?
+          </h1>
+          <div className="flex gap-4 text-sm">
+            <button
+              onClick={() => {
+                changeAudioEnabled(true);
+                setHasInteracted(true);
+              }}
+              className="px-4 py-2 bg-linear-to-r/hsl from-[#D0104C] to-[#F75C2F] shadow-lg rounded-lg"
+            >
+              Yes, with sound
+            </button>
+            <button
+              onClick={() => {
+                changeAudioEnabled(false);
+                setHasInteracted(true);
+              }}
+              className="px-4 py-2 bg-linear-to-r/hsl from-[#707C74] to-[#72636E] shadow-lg rounded-lg"
+            >
+              No, silent
+            </button>
+          </div>
         </div>
       </div>
     );
